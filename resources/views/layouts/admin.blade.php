38d3e0f4ce8d6ed33f50b8a34ee01a713e1bdcc1
Surya Dinarta <Surya.10rpl1.2019@gmail.com>
2023-11-12 22:37:46 +0700

    manifest update 2

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -31,7 +31,7 @@
 
     <!-- PWA  -->
     <meta name="theme-color" content="#6777ef" />
-    <link rel="apple-touch-icon" href="{{ asset('logo.PNG') }}">
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
     <link rel="manifest" href="{{ asset('/manifest.json') }}">
     {{-- <link rel="manifest" href="{{ asset('favicons/manifest.json') }}"> --}}
     <meta name="msapplication-TileColor" content="#ffffff">
